0e1c63a2577a47d1cfe1de98fb41bf447267db50
Fectel <HectorGamador@gmail.com>
2023-02-28 15:27:44 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'taco-fit-2023',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
